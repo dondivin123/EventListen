44ed8ebd67b80e3ddb40073c718de1fe545d22bc
dondivin123 <ndayihagazekodivin100@gmail.com>
2024-12-12 02:05:28 -0800

    using onclick

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-function handleclick(){
-    console.log('Button clicked')
+const handlelogin = () =>{
+    const fullName = document.getElementById("fullName").value
+    const email = document.getElementById("email").value
+    const passWord = document.getElementById("passWord").value
+
+    console.log(fullName);
+    console.log(email);
+    console.log(passWord);
+    
 }
