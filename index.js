c13382214bff3297a0f366088cfec3e40add01d3
dondivin123 <ndayihagazekodivin100@gmail.com>
2024-12-12 01:34:34 -0800

    by using object

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-const handleSum = () =>{
-    const num1= document.getElementById("num1").value
-    const num2= document.getElementById("num2").value
-    const resultContainer = document.getElementById('result')
-
-    const sum = Number(num1) + Number(num2)
-
-    if(num1=== "") {
-        resultContainer.innerHTML= "please provide number in input"
-        return;    
-    }
-    if(num2=== "") {
-        resultContainer.innerText= "please don't click here"
-        return;
-    }
-    resultContainer.innerHTML = (`The sum is: ${sum}`)
+const motor= {
+    name: "TVS",
+    plate: "RAB 23744",
+    owner: "jean",
 }
+localStorage.setItem('motorkey',JSON.stringify(motor))
+
+const lewis= localStorage.getItem('motorkey')
+console.log(JSON.parse(lewis))
