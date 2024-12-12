569f6a9b46f541c5ccca4820597a043d7e9416e2
dondivin123 <ndayihagazekodivin100@gmail.com>
2024-12-12 01:22:34 -0800

    by using sum

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-let k = 0
-document.getElementById('counter').innerHTML=k
+const handleSum = () =>{
+    const num1= document.getElementById("num1").value
+    const num2= document.getElementById("num2").value
+    const resultContainer = document.getElementById('result')
 
-const handleDecrement=()=>{
-    document.getElementById('counter').innerHTML=k-=1
-}
-const handleIcrement =()=>{
-    document.getElementById('counter').innerHTML=k+=1
+    const sum = Number(num1) + Number(num2)
+
+    if(num1=== "") {
+        resultContainer.innerHTML= "please provide number in input"
+        return;    
+    }
+    if(num2=== "") {
+        resultContainer.innerText= "please don't click here"
+        return;
+    }
+    resultContainer.innerHTML = (`The sum is: ${sum}`)
 }
